46d20a3628ebd58cc0c00b774068c7ecb60f6d7d
Soham Mitra <mitra.soham53@gmail.com>
2026-08-30 04:56:16 +0530

    updated the DB connection code to use env

--- backend/internal/platform/db/db.go
+++ backend/internal/platform/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
 	_ "github.com/lib/pq"
@@ -11,7 +12,10 @@ import (
 func InitializeDB() *sql.DB {
 	db_config := make(map[string]string)
 	db_config["hostname"] = os.Getenv("DB_HOST")
-	db, err := sql.Open("postgres", "hostname=localhost dbname=system_design user=kokurou password=hello@User123 sslmode=disable")
+	db_config["dbname"] = os.Getenv("DB_NAME")
+	db_config["user"] = os.Getenv("DB_USER")
+	db_config["password"] = os.Getenv("DB_PASS")
+	db, err := sql.Open("postgres", fmt.Sprintf("hostname=localhost dbname=system_design user=kokurou password=hello@User123 sslmode=disable", db_config["hostname"], db_config["dbname"], db_config["user"], db_config["password"]))
 	if err != nil {
 		logrus.Error("DB connection failed with error", err)
 		return nil
