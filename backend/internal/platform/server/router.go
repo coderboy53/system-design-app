34fd870960e0b3eee1d56503812d470172bdfb52
Soham Mitra <mitra.soham53@gmail.com>
2026-08-31 02:23:39 +0530

    setup the initial skeleton of the server

--- backend/internal/platform/server/router.go
+++ backend/internal/platform/server/router.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handlerRoutes(r *gin.Engine) {
+	// temp root endpoint for now
+	r.GET(
+		"/",
+		func(c *gin.Context) {
+			c.JSON(
+				http.StatusOK,
+				gin.H{
+					"message": "Welcome to app",
+				},
+			)
+		},
+	)
+}
--- backend/internal/platform/server/server.go
+++ backend/internal/platform/server/server.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"github.com/coderboy53/system-design-app/internal/platform/db"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+// initializes the gin Engine, then sets up the routes and initializes connection to DB
+func StartServer() {
+	r := gin.Default()
+	handlerRoutes(r)
+	db.InitializeDB()
+	err := r.Run("localhost:9000")
+	if err != nil {
+		logrus.Fatal("Failed to start gin engine with error: ", err)
+	}
+}
