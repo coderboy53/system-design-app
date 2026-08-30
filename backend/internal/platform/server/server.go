d345eb1c61357afe219e0edaf707eef2b66ab658
Soham Mitra <mitra.soham53@gmail.com>
2026-08-31 04:25:36 +0530

    added redis initialization code and added it to start server process

--- backend/internal/platform/server/server.go
+++ backend/internal/platform/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"github.com/coderboy53/system-design-app/internal/platform/db"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
@@ -11,8 +13,19 @@ func StartServer() {
 	r := gin.Default()
 	handlerRoutes(r)
 	db.InitializeDB()
+	redisStore := initializeRedis()
+	r.Use(sessions.Sessions("appsessions", redisStore))
 	err := r.Run("localhost:9000")
 	if err != nil {
 		logrus.Fatal("Failed to start gin engine with error: ", err)
 	}
+}
+
+func initializeRedis() redis.Store {
+	store, err := redis.NewStoreWithDB(20, "tcp", "localhost:6379", "", "", "")
+	if err != nil {
+		logrus.Error("Failed to create session store with error: ", err)
+		return nil
+	}
+	return store
 }
