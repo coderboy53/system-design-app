4f13534a21dd84abb0a0062e28fe066e348c8f80
Soham Mitra <mitra.soham53@gmail.com>
2026-08-31 02:23:15 +0530

    updated the connection string to use the env variables

--- backend/internal/platform/db/db.go
+++ backend/internal/platform/db/db.go
@@ -9,13 +9,14 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// initializes the connection with local postgres reading env variables
 func InitializeDB() *sql.DB {
 	db_config := make(map[string]string)
 	db_config["hostname"] = os.Getenv("DB_HOST")
 	db_config["dbname"] = os.Getenv("DB_NAME")
 	db_config["user"] = os.Getenv("DB_USER")
 	db_config["password"] = os.Getenv("DB_PASS")
-	db, err := sql.Open("postgres", fmt.Sprintf("hostname=localhost dbname=system_design user=kokurou password=hello@User123 sslmode=disable", db_config["hostname"], db_config["dbname"], db_config["user"], db_config["password"]))
+	db, err := sql.Open("postgres", fmt.Sprintf("hostname=%s dbname=%s user=%s password=%s sslmode=disable", db_config["hostname"], db_config["dbname"], db_config["user"], db_config["password"]))
 	if err != nil {
 		logrus.Error("DB connection failed with error", err)
 		return nil
