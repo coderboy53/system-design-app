67fc59d86bff9259a39cbbbdb179190f30c38e70
Soham Mitra <mitra.soham53@gmail.com>
2026-08-31 06:45:08 +0530

    middleware use is supposed to be defined before our routes are called, changed the positioning and added the redis auth key for session consistency failure bug

--- backend/internal/platform/server/server.go
+++ backend/internal/platform/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"os"
+
 	"github.com/coderboy53/system-design-app/internal/platform/db"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/redis"
@@ -11,10 +13,10 @@ import (
 // initializes the gin Engine, then sets up the routes and initializes connection to DB
 func StartServer() {
 	r := gin.Default()
-	handlerRoutes(r)
 	db.InitializeDB()
 	redisStore := initializeRedis()
 	r.Use(sessions.Sessions("appsessions", redisStore))
+	handlerRoutes(r)
 	err := r.Run("localhost:9000")
 	if err != nil {
 		logrus.Fatal("Failed to start gin engine with error: ", err)
@@ -22,7 +24,7 @@ func StartServer() {
 }
 
 func initializeRedis() redis.Store {
-	store, err := redis.NewStoreWithDB(20, "tcp", "localhost:6379", "", "", "")
+	store, err := redis.NewStoreWithDB(20, "tcp", "localhost:6379", "", "", "", []byte(os.Getenv("REDIS_AUTH_KEY")))
 	if err != nil {
 		logrus.Error("Failed to create session store with error: ", err)
 		return nil
