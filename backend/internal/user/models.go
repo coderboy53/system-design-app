b8f6c668a82a8dbee3701d8488e214394ea3098c
Soham Mitra <mitra.soham53@gmail.com>
2026-09-01 05:59:37 +0530

    created the user model

--- backend/internal/user/models.go
+++ backend/internal/user/models.go
@@ -1,1 +1,9 @@
 package user
+
+type User struct {
+	Subject    string `json:"sub"`
+	Name       string `json:"name"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Email      string `json:"email"`
+}
