5dcab07fa93cf95b5c061667977a5cc797fba4f0
Soham Mitra <mitra.soham53@gmail.com>
2026-09-03 05:34:22 +0530

    initialized DB operation file, and created struct and constructor to receive the DB connection from main and use it

---
+++ backend/internal/modules/db.go
@@ -0,0 +1,9 @@
+package modules
+
+import "database/sql"
+
+//type Db *sql.DB
+
+func GetModules(db *sql.DB) {
+
+}
--- backend/internal/modules/handlers.go
+++ backend/internal/modules/handlers.go
@@ -1,1 +1,31 @@
 package modules
+
+import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	Db *sql.DB
+}
+
+func NewHandlers(db *sql.DB) *Handler {
+	return &Handler{Db: db}
+}
+
+func (h *Handler) GetModules(c *gin.Context) {
+
+}
+
+func (h *Handler) GetModule(c *gin.Context) {
+
+}
+
+func GetTopics(c *gin.Context) {
+
+}
+
+func GetTopic(c *gin.Context) {
+
+}
