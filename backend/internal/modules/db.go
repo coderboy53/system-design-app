b9bf5840cfa1ba7ca24e074685f7e8b480090f15
Soham Mitra <mitra.soham53@gmail.com>
2026-09-08 21:45:36 +0530

    wrote logic for getting modules and initialized functions to add modules and topics to db

--- backend/internal/modules/db.go
+++ backend/internal/modules/db.go
@@ -1,9 +1,40 @@
 package modules
 
-import "database/sql"
+import (
+	"database/sql"
 
-//type Db *sql.DB
+	"github.com/sirupsen/logrus"
+)
 
-func GetModules(db *sql.DB) {
+// tmp functions for populating data
+func AddModules(db *sql.DB, body Module) {
 	
+
+}
+
+func AddTopics(db *sql.DB, body Topic) {
+
+}
+
+func GetModules(db *sql.DB) ([]Module, error) {
+	modules := make([]Module, 9)
+	rows, err := db.Query("SELECT * FROM modules;")
+	if err != nil {
+		logrus.Error("Error retrieving rows: ", err)
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var module Module
+		if err := rows.Scan(&module.Id, &module.Title, &module.ModuleOrder, &module.TopicCount, &module.Overview); err != nil {
+			logrus.Error("Error scanning rows: ", err)
+			return nil, err
+		}
+		modules = append(modules, module)
+	}
+	if !rows.NextResultSet() {
+		logrus.Error("Expected more rows: ", rows.Err())
+		return nil, err
+	}
+	return modules, nil
 }
