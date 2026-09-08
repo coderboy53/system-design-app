43872c4953493b68b113d931d006ab4649c535fb
Soham Mitra <mitra.soham53@gmail.com>
2026-09-08 21:45:07 +0530

    wrote handlers for tmp endpoints to add data

--- backend/internal/modules/handlers.go
+++ backend/internal/modules/handlers.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,15 +17,24 @@ func NewHandlers(db *sql.DB) *Handler {
 
 // tmp endpoints to just push data
 func (h *Handler) AddModules(c *gin.Context) {
-
+	var body Module
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message":"Invalid body passed"})
+	}
+	AddModules(h.Db, body)
 }
 
 func (h *Handler) AddTopics(c *gin.Context) {
-
+	var body Topic
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message":"Invalid body passed"})
+	}
+	AddTopics(h.Db, body)
 }
 
+// production endpoints start here
 func (h *Handler) GetModules(c *gin.Context) {
-
+	
 }
 
 func (h *Handler) GetModule(c *gin.Context) {
